c3b3781f8d63f5690be73943c8a682cc4d640a6d
pharaon50laughing <dennagutowskivbm7k@gmail.com>
2025-06-06 19:00:21 -0400

    Add files via upload

---
+++ 7kpXSc.c
@@ -0,0 +1,6 @@
+// J3obrAqMTb
+#include <stdio.h>
+int main() {
+    printf("Hello from function dac5bfd8fcbc4f62ab5327b61ed20726_20250606142047436744!\n");
+    return 0;
+}
---
+++ OnXxIB.rs
@@ -0,0 +1,4 @@
+// J3obrAqMTb
+fn main() {
+    println!("Hello from function dac5bfd8fcbc4f62ab5327b61ed20726_20250606142047436744!");
+}
